7d721ef284fe225459652c17cea693af5cd0bc3b
Martin Adams <martin@martinadams.com>
2016-09-16 10:15:54 +1200

    Basic working list and profiles

--- app.js
+++ app.js
@@ -1,18 +1,44 @@
 var express = require('express')
 var exphbs = require('express-handlebars')
 var nodemon = require('nodemon')
-var knex = require('knex')
+var config = require('./knexfile').development
+var knex = require('knex')(config)
 
 var app = express()
 
- app.engine('handlebars', exphbs({defaultLayout: 'main'}))
- app.set('view engine', 'handlebars')
+app.engine('handlebars', exphbs({
+	defaultLayout: 'main'
+}))
+app.set('view engine', 'handlebars')
 
 
- app.get('/', function (req, res) {
-  res.send('hello')
- })
+app.get('/', function(req, res) {
+	knex('pokemon')
+		.select()
+		.then(
+			function(pokemon) {
+				res.render('list', {
+					pokemon: pokemon
+				})
+			}
+		)
+		.catch(function(err) {
+			console.log(err)
+		})
+})
 
-
+app.get('/:id', function(req, res) {
+   knex('pokemon')
+   .select()
+   .then(function (pokemon) {
+      var uniquePoke = pokemon.filter(function (uniquePoke) {
+         return uniquePoke.id === Number(req.params.id)
+      })[0]
+      res.render('pokeProfile', uniquePoke)
+   })
+   .catch(function (err) {
+      console.log(err)
+   })
+})
 
 module.exports = app

9bcdcde27d2f6e22b6ea77df11aedd6b80345ff3
Martin Adams <martin@martinadams.com>
2016-09-17 10:31:58 +1200

    Small adjustments

--- app.js
+++ app.js
@@ -16,10 +16,25 @@ app.set('view engine', 'handlebars')
 // serve the files in /public
 app.use(express.static(path.join(__dirname, 'public')))
 
+function getAllPokes() {
+	return knex('pokemon').select()
+}
+
+function getPoke(id) {
+	return knex('pokemon')
+		.join('eyedees', 'eyedees.pokemon_id', '=', 'pokemon.id')
+		.join('types', 'types.id', '=', 'eyedees.type_id')
+		.where('pokemon.id', id)
+}
+
+function getPokeByType(type) {
+	return knex('eyedees')
+		.join('pokemon', 'pokemon.id', '=', 'eyedees.pokemon_id')
+		.where('eyedees.type_id', type)
+}
 
 app.get('/', function(req, res) {
-	knex('pokemon')
-		.select()
+	getAllPokes()
 		.then(
 			function(pokemon) {
 				res.render('list', {
@@ -32,50 +47,38 @@ app.get('/', function(req, res) {
 		})
 })
 
-function getAllPokes() {
-   return knex('pokemon').select()
-}
-
-function getPoke(id) {
-   return knex('pokemon')
-   .join('eyedees', 'eyedees.pokemon_id', '=', 'pokemon.id')
-   .join('types', 'types.id', '=', 'eyedees.type_id')
-   .where('pokemon.id', id)
-}
-
-function getPokeByType(type) {
-   return knex('eyedees')
-   .join('pokemon', 'pokemon.id', '=', 'eyedees.pokemon_id')
-   .where('eyedees.type_id', type)
-}
-
 app.get('/:id', function(req, res) {
-   var unique, typeStrings
-   getPoke(req.params.id)
-   .then(function (uniquePoke) {
-      unique = uniquePoke[0]
-      var typeIds = uniquePoke.map(function (obj) {
-         return obj.type_id
-      })
-      typeStrings = uniquePoke.map(function (obj) {
-         return obj.type
-      })
-      var pokePromises = typeIds.map(getPokeByType)
-      return Promise.all(pokePromises)
-   })
-   .then(function (result) {
-      var pokeRelatives = result[0]
-      for (var i = 0; i < result.length - 1; i++) {
-         pokeRelatives = pokeRelatives.concat(result[i+1])
-      }
-      var pokeRelativeNames = pokeRelatives.map(function (obj) {
-         return obj.name
-      })
-      res.render('pokeProfile', {name: unique.name, image: unique.image, type: typeStrings, pokeRelativeName: pokeRelativeNames})
-   })
-   .catch(function (err) {
-      console.log(err)
-   })
+	var unique, typeStrings
+	getPoke(req.params.id)
+		.then(function(uniquePoke) {
+			unique = uniquePoke[0]
+			var typeIds = uniquePoke.map(function(obj) {
+				return obj.type_id
+			})
+			typeStrings = uniquePoke.map(function(obj) {
+				return obj.type
+			})
+			var pokePromises = typeIds.map(getPokeByType)
+			return Promise.all(pokePromises)
+		})
+		.then(function(result) {
+			var pokeRelatives = result[0]
+			for (var i = 0; i < result.length - 1; i++) {
+				pokeRelatives = pokeRelatives.concat(result[i + 1])
+			}
+			var pokeRelativeNames = pokeRelatives.map(function(obj) {
+				return obj.name
+			})
+			res.render('pokeProfile', {
+				name: unique.name,
+				image: unique.image,
+				type: typeStrings,
+				pokeRelativeName: pokeRelativeNames
+			})
+		})
+		.catch(function(err) {
+			console.log(err)
+		})
 })
 
 module.exports = app
